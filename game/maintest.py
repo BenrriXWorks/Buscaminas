9e7fe1e4ae4a823b1ab1c7ff2c25ded5be6cf2db
BenrriXWorks <BenrriXWorks@gmail.com>
2023-12-18 21:08:11 -0300

    Feature: Lectura de argumentos

--- game/maintest.py
+++ game/maintest.py
@@ -1,20 +1,37 @@
 import tkinter as tk
 from src.Mapa.Mapa import MapaSingleton
 from src.StateHandler.StateHandler import StateHandlerSingleton
+import sys
 
-rows, columns, bombs = 20, 20, 80
+class Main:
 
-root = tk.Tk()
-root.geometry("800x800")
+    def main():
 
-gameStateHandler = StateHandlerSingleton()
-gameStateHandler.setMapAttributes(rows, columns, bombs)
+        # Leer los argumentos
+        try:
+            if (len(sys.argv) != 4):
+                raise ValueError("La llamada debe ser $ game.py <filas> <columnas> <bombas>")
+            rows, columns, bombs = map(int, sys.argv[1:])
+        except Exception as e:
+            print("Hubo un error:", e)
+            return
 
-mapa = MapaSingleton()
-mapa.create(root, rows, columns, bombs)
-mapa.pack(fill=tk.BOTH, expand=True)
+        # Crear la ventana
+        root = tk.Tk()
+        root.geometry("800x800")
 
-# Forzar la actualización de la ventana antes de comenzar el bucle principal
-root.update()
+        # Iniciar el manejador del estado de juego
+        gameStateHandler = StateHandlerSingleton()
+        gameStateHandler.setMapAttributes(rows, columns, bombs)
 
-root.mainloop()
+        # Crear el mapa de celdas
+        mapa = MapaSingleton()
+        mapa.create(root, rows, columns, bombs)
+        mapa.pack(fill=tk.BOTH, expand=True)
+
+        # Iniciar el loop de renderizado de tkinter
+        root.mainloop()
+
+if __name__ == "__main__":
+
+    Main.main()
